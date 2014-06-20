251419fdb616256f7f4a64348f218ccbede47475
steven84 <mayuqian0910@gmail.com>
2014-06-20 16:47:53 +0800

    create new data frame,prepare for chart

--- 20140619.R
+++ 20140619.R
@@ -2,9 +2,20 @@
 ##加载foreign包
 library(foreign)
 
-#将spss数据导入R
+##将spss数据导入R
 Q15_Data <- read.spss("F:/GitHub/learn-R/Pilot_Q12_Survey_Data_0619.sav")
 
 #summary
 summary(Q15_Data)
+Q15_Data
 
+#Data frame 建立
+attach(Q15_Data)
+AnalysisData <- data.frame(ID,Q1n,Q2n,Q3n,Q4n,Q5n,Q6n,Q7n,Q8n,Q9n,Q10n,
+                           Q11n,Q12n,Q13n,Q14n,Q15n,Q26n,Q27n,Q28n,
+                          Q29n,Q31)
+detach(Q15_Data)
+
+library(ggplot2)
+library(gcookbook)
+barplot(AnalysisData$Q1n)
